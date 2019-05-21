449c6e138197bf3190f2348905045ad6f950d8d2
komal <komal@LAPTOP-GPSG7DIK>
2019-05-22 11:13:24 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_data_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine(-1 + 4 * 6);
+                // The answer of this statement is 23
+                Console.WriteLine((35 + 5) % 7);
+                // The answer of this statement is 5
+                Console.WriteLine(14 + -4 * 6 / 11);
+                // The answer of this statement is 12
+                Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+                // The answer of this statement is 3
+            }
+        }
+    }
+}
